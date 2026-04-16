df8ac8bbc5927168f7ad8a2788f190aa33444c11
Nixwzy <guifla330@gmail.com>
2026-04-16 02:47:30 -0300

    feat: add global error handler and native env file support

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from 'express';
 import cors from 'cors';
 import { routes } from './routes/main.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const server = express();
 server.use(cors());
@@ -12,7 +13,12 @@ server.use(express.json());
 
 server.use(routes);
 
+server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
-  console.log('Server running successfully');
+  console.log('Server running successfully on port ' + port);
 });
